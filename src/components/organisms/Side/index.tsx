04971397aaffb8bf89d9882a2ad7abc743f64c40
yuyake0084 <yuyake0084@gmail.com>
2022-04-30 18:17:55 +0900

    Sideを実装

---
+++ src/components/organisms/Side/index.tsx
@@ -0,0 +1,10 @@
+import { Nav } from '~/components/atoms/Nav';
+import * as Styled from './style';
+
+export const Side = () => {
+  return (
+    <Styled.Aside>
+      <Nav />
+    </Styled.Aside>
+  );
+};
---
+++ src/components/organisms/Side/style.tsx
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+import { COLORS } from '~/consts/style';
+
+export const Aside = styled.aside`
+  width: 100%;
+  max-width: 300px;
+  padding: 20px 0;
+  background-color: ${COLORS.GRAY100.code};
+`;
