a6c312249a89be322819e4fdb19bb6a356417a0c
yuyake0084 <yuyake0084@gmail.com>
2022-05-03 12:50:42 +0900

    TaskDetailをdynamic importするように修正

--- src/components/templates/TaskDetail/index.tsx
+++ src/components/templates/TaskDetail/index.tsx
@@ -1,51 +1,8 @@
-import type { ReactNode } from 'react';
+import dynamic from 'next/dynamic';
+import type { TaskDetailProps } from './TaskDetail';
 
-import * as Styled from './style';
-import { FaInfoCircle } from 'react-icons/fa';
-import { ICON_SIZES } from '~/consts/style';
-import type { Curriculum } from '~/consts/curriculums';
+const DynamicTaskDetail = dynamic(() => import('./TaskDetail'), { ssr: false });
 
-export type DetailProps = Curriculum & {
-  children: ReactNode;
-};
-
-/**
- * 課題の詳細を表示するコンポーネント
- * @param props {@link DetailProps}
- */
-export const TaskDetail = (props: DetailProps) => {
-  const { title, description, requirements, example, children } = props;
-
-  return (
-    <>
-      <Styled.CurriculumTitle>{title}</Styled.CurriculumTitle>
-
-      {description}
-
-      <Styled.RequirementListBox>
-        <Styled.RequirementHeading>
-          <Styled.RequirementIcon>
-            <FaInfoCircle size={ICON_SIZES.L} />
-          </Styled.RequirementIcon>
-          仕様
-        </Styled.RequirementHeading>
-        <ul>
-          {requirements.map((requirement) => (
-            <li key={requirement}>{requirement}</li>
-          ))}
-        </ul>
-      </Styled.RequirementListBox>
-
-      <Styled.Section>
-        <Styled.Title>サンプル</Styled.Title>
-        <Styled.Contents>{example}</Styled.Contents>
-      </Styled.Section>
-
-      <Styled.Section>
-        <Styled.Title>実装</Styled.Title>
-        <Styled.Annotation>サンプルを参考に実装してください</Styled.Annotation>
-        <Styled.Contents>{children}</Styled.Contents>
-      </Styled.Section>
-    </>
-  );
+export const TaskDetail = (props: TaskDetailProps) => {
+  return <DynamicTaskDetail {...props} />;
 };
