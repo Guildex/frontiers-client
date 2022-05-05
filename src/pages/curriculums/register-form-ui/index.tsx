63e8bcd8e530a858f3806a6041c38f10bad7bb93
yuyake0084 <yuyake0084@gmail.com>
2022-05-05 20:08:02 +0900

    register-form => register-form-ui

--- src/pages/curriculums/register-form-ui/index.tsx
+++ src/pages/curriculums/register-form-ui/index.tsx
@@ -2,14 +2,14 @@ import type { NextPage } from 'next';
 import { TaskDetail } from '~/components/templates/TaskDetail';
 import { CURRICULUMS } from '~/consts/curriculums';
 
-const RegisterForm: NextPage = () => {
+const RegisterFormUI: NextPage = () => {
   return (
     <>
-      <TaskDetail {...CURRICULUMS.REGISTER_FORM}>
+      <TaskDetail {...CURRICULUMS.REGISTER_FORM_UI}>
         {/* 回答はこちらに実装してください */}
       </TaskDetail>
     </>
   );
 };
 
-export default RegisterForm;
+export default RegisterFormUI;
