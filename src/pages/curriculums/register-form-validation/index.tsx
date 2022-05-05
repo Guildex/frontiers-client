f1430210cc7453934806b432594b08e8b5720d2f
yuyake0084 <yuyake0084@gmail.com>
2022-05-05 21:03:19 +0900

    バリデーション用コンポーネント実装

---
+++ src/pages/curriculums/register-form-validation/index.tsx
@@ -0,0 +1,12 @@
+import { TaskDetail } from '~/components/templates/TaskDetail';
+import { CURRICULUMS } from '~/consts/curriculums';
+
+const RegisterFormValidation = () => {
+  return (
+    <TaskDetail {...CURRICULUMS.REGISTER_FORM_VALIDATION}>
+      {/* 回答はこちらに実装してください */}
+    </TaskDetail>
+  );
+};
+
+export default RegisterFormValidation;
