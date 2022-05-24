727dfb29c0195785a7d328a7b6ea09c15ff05d98
yuyake0084 <yuyake0084@gmail.com>
2022-05-24 10:06:23 +0900

    eslint追加

---
+++ .eslintrc.js
@@ -0,0 +1,21 @@
+module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: './tsconfig.json',
+  },
+  plugins: ['@typescript-eslint'],
+  extends: [
+    'next/core-web-vitals',
+    'eslint:recommended',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+    'prettier',
+  ],
+  rules: {
+    '@typescript-eslint/no-floating-promises': 'error',
+    'react/react-in-jsx-scope': 'off',
+  },
+};
