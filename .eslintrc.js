5ae3c0fdd767140f97eb618761406eb27b911f0e
yuyake0084 <yuyake0084@gmail.com>
2022-06-07 13:18:06 +0900

    rules追加

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,7 +4,7 @@ module.exports = {
   parserOptions: {
     project: './tsconfig.json',
   },
-  plugins: ['@typescript-eslint'],
+  plugins: ['@typescript-eslint', 'import', 'unused-imports'],
   extends: [
     'next/core-web-vitals',
     'eslint:recommended',
@@ -15,7 +15,57 @@ module.exports = {
     'prettier',
   ],
   rules: {
+    // @typescript-eslint
     '@typescript-eslint/no-floating-promises': 'error',
+    '@typescript-eslint/naming-convention': [
+      'error',
+      {
+        selector: 'parameter',
+        format: ['camelCase'],
+      },
+      {
+        selector: 'variable',
+        types: ['boolean'],
+        format: ['PascalCase'],
+        prefix: ['is', 'has'],
+      },
+      {
+        selector: 'typeAlias',
+        format: ['PascalCase'],
+      },
+    ],
+    '@typescript-eslint/no-unused-vars': 'off',
+    '@typescript-eslint/consistent-type-imports': 'error',
+
+    // react
     'react/react-in-jsx-scope': 'off',
+
+    // unused-imports
+    'unused-imports/no-unused-imports': 'error',
+
+    // import
+    'import/order': [
+      'error',
+      {
+        groups: [
+          ['builtin', 'external', 'internal'],
+          'parent',
+          'sibling',
+          'index',
+          'object',
+        ],
+        pathGroups: [
+          {
+            pattern: '~/**',
+            group: 'parent',
+            position: 'before',
+          },
+        ],
+        alphabetize: {
+          order: 'asc',
+        },
+        'newlines-between': 'always',
+      },
+    ],
   },
 };
