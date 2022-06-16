522d7e378aa60e790b178a4c59d52926f8d5b4b8
yuyake0084 <yuyake0084@gmail.com>
2022-06-16 16:37:53 +0900

    Documentを最適化

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -2,16 +2,31 @@ import Document from 'next/document';
 import type { DocumentContext } from 'next/document';
 import { ServerStyleSheet } from 'styled-components';
 
-export default class AppDocument extends Document {
+export default class MyDocument extends Document {
   static async getInitialProps(ctx: DocumentContext) {
     const sheet = new ServerStyleSheet();
-    const page = ctx.renderPage(
-      // eslint-disable-next-line @typescript-eslint/naming-convention
-      (App) => (props) => sheet.collectStyles(<App {...props} />),
-    );
+    const originalRenderPage = ctx.renderPage;
+    const initialProps = await Document.getInitialProps(ctx);
 
-    const styleTags = sheet.getStyleElement();
+    try {
+      ctx.renderPage = () =>
+        originalRenderPage({
+          // eslint-disable-next-line @typescript-eslint/naming-convention
+          enhanceApp: (App) => (props) =>
+            sheet.collectStyles(<App {...props} />),
+        });
 
-    return { ...page, styleTags };
+      return {
+        ...initialProps,
+        styles: [
+          <>
+            {initialProps.styles}
+            {sheet.getStyleElement()}
+          </>,
+        ],
+      };
+    } finally {
+      sheet.seal();
+    }
   }
 }
