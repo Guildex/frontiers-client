bcfa2806dd077984a1bd14c0c2d09ffc9ea33256
yuyake0084 <yuyake0084@gmail.com>
2022-06-16 17:05:00 +0900

    domain変更

--- src/consts/app.ts
+++ src/consts/app.ts
@@ -1,5 +1,5 @@
 export const SITE_NAME = 'Next React Workbook';
-export const DOMAIN = 'react-next-curriculum.vercel.app';
+export const DOMAIN = 'next-react-workbook-henna.vercel.app';
 export const BASE_URL = `https://${DOMAIN}/`;
 
 export const OPEN_GRAPH_IMAGE_WIDTH = 1200;
