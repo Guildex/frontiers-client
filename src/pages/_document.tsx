384a25f0ec7bbe2e483f2cbbfa9285b0b13b1bb1
yuyake0084 <yuyake0084@gmail.com>
2022-06-18 18:28:00 +0900

    レイアウトシフト解消

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,4 +1,5 @@
-import Document from 'next/document';
+import { CssBaseline } from '@nextui-org/react';
+import Document, { Html, Head, Main, NextScript } from 'next/document';
 import type { DocumentContext } from 'next/document';
 import { ServerStyleSheet } from 'styled-components';
 
@@ -18,15 +19,22 @@ export default class MyDocument extends Document {
 
       return {
         ...initialProps,
-        styles: [
-          <>
-            {initialProps.styles}
-            {sheet.getStyleElement()}
-          </>,
-        ],
+        styles: [initialProps.styles, sheet.getStyleElement()],
       };
     } finally {
       sheet.seal();
     }
+  }
+
+  render() {
+    return (
+      <Html lang="ja">
+        <Head>{CssBaseline.flush()}</Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
   }
 }
