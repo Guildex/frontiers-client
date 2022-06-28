2222bd7e87e9040c507f62ba9cb0b96e061b40b8
yuyake0084 <yuyake0084@gmail.com>
2022-06-28 10:51:46 +0900

    パッケージ化されたESLintを利用するように修正

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,71 +1,7 @@
 module.exports = {
   root: true,
-  parser: '@typescript-eslint/parser',
   parserOptions: {
     project: './tsconfig.json',
   },
-  plugins: ['@typescript-eslint', 'import', 'unused-imports'],
-  extends: [
-    'next/core-web-vitals',
-    'eslint:recommended',
-    'plugin:@typescript-eslint/eslint-recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:react/recommended',
-    'plugin:react-hooks/recommended',
-    'prettier',
-  ],
-  rules: {
-    // @typescript-eslint
-    '@typescript-eslint/no-floating-promises': 'error',
-    '@typescript-eslint/naming-convention': [
-      'error',
-      {
-        selector: 'parameter',
-        format: ['camelCase'],
-      },
-      {
-        selector: 'variable',
-        types: ['boolean'],
-        format: ['PascalCase'],
-        prefix: ['is', 'has'],
-      },
-      {
-        selector: 'typeAlias',
-        format: ['PascalCase'],
-      },
-    ],
-    '@typescript-eslint/no-unused-vars': 'off',
-    '@typescript-eslint/consistent-type-imports': 'error',
-
-    // react
-    'react/react-in-jsx-scope': 'off',
-
-    // unused-imports
-    'unused-imports/no-unused-imports': 'error',
-
-    // import
-    'import/order': [
-      'error',
-      {
-        groups: [
-          ['builtin', 'external', 'internal'],
-          'parent',
-          'sibling',
-          'index',
-          'object',
-        ],
-        pathGroups: [
-          {
-            pattern: '~/**',
-            group: 'parent',
-            position: 'before',
-          },
-        ],
-        alphabetize: {
-          order: 'asc',
-        },
-        'newlines-between': 'always',
-      },
-    ],
-  },
+  extends: ['@guildex/eslint-config', 'next/core-web-vitals'],
 };
