c689d8407ba16c6e818919523afe950fb33b742a
yuyake0084 <yuyake0084@gmail.com>
2022-07-02 14:30:19 +0900

    exampleをアップデート

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
+const composePlugins = require('next-compose-plugins');
+const withTM = require('next-transpile-modules')([
+  '@guildex/react-next-curriculum-example',
+]);
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
 };
 
-module.exports = nextConfig;
+module.exports = composePlugins([withTM], nextConfig);
