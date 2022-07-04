4cc77d11a568d9129ad685461be1baed3ed95060
yuyake0084 <yuyake0084@gmail.com>
2022-07-04 19:53:37 +0900

    next-transpile-modulesを削除

--- next.config.js
+++ next.config.js
@@ -1,11 +1,8 @@
 const composePlugins = require('next-compose-plugins');
-const withTM = require('next-transpile-modules')([
-  '@guildex/react-next-curriculum-example',
-]);
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
 };
 
-module.exports = composePlugins([withTM], nextConfig);
+module.exports = composePlugins([], nextConfig);
