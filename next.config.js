5834b27160d262b38297e3202325505fc9da448e
yuyake0084 <yuyake0084@gmail.com>
2022-07-26 20:40:56 +0900

    MDXに関連するライブラリ導入

--- next.config.js
+++ next.config.js
@@ -1,8 +1,17 @@
 const composePlugins = require('next-compose-plugins');
+const withMDX = require('@next/mdx')({
+  extension: /\.mdx?$/,
+  options: {
+    providerImportSource: '@mdx-js/react',
+    remarkPlugins: [],
+    rehypePlugins: [],
+  },
+});
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  pageExtensions: ['ts', 'tsx', 'mdx'],
 };
 
-module.exports = composePlugins([], nextConfig);
+module.exports = composePlugins([withMDX], nextConfig);
