053db5bc08cbe9e07add8916551a661cc7c574e1
yuyake0084 <yuyake0084@gmail.com>
2022-09-11 13:26:08 +0900

    レイアウトシフト解消

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -7,7 +7,6 @@ export default class MyDocument extends Document {
   static async getInitialProps(ctx: DocumentContext) {
     const sheet = new ServerStyleSheet();
     const originalRenderPage = ctx.renderPage;
-    const initialProps = await Document.getInitialProps(ctx);
 
     try {
       ctx.renderPage = () =>
@@ -16,9 +15,16 @@ export default class MyDocument extends Document {
           enhanceApp: (App) => (props) => sheet.collectStyles(<App {...props} />),
         });
 
+      const initialProps = await Document.getInitialProps(ctx);
+
       return {
         ...initialProps,
-        styles: [initialProps.styles, sheet.getStyleElement()],
+        styles: (
+          <>
+            {initialProps.styles}
+            {sheet.getStyleElement()}
+          </>
+        ),
       };
     } finally {
       sheet.seal();
